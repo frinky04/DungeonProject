2f26870220721198049279136ccb65979a0b2535
Frinky <cuddlysharkyt@gmail.com>
2021-08-23 10:25:12 +0930

    Init

---
+++ DungeonThing/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace DungeonThing
+{
+    class Enemy
+    {
+        public Vector2 position = new Vector2(0, 0);
+    }
+}
---
+++ DungeonThing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DungeonThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+        }
+    }
+}
